1bfc7ae563f6b77d31197ec5147c4cc600ca19da
Jordan <jordanace83@gmail.com>
2021-01-06 17:15:49 -0600

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,40 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // var list = {{head : {value : 4, next : {value: 5, next: null}}}, {tail: {value: 5, next: null}}}
+
   list.addToTail = function(value) {
+    var lastnode = list.head;
+
+    if (!list.head) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+      while (lastnode.next) {
+        lastnode = lastnode.next;
+      }
+      lastnode.next = Node(value);
+      list.tail = lastnode.next;
+    }
   };
 
   list.removeHead = function() {
+    var storedHead = list.head.value;
+    list.head = list.head.next;
+    return storedHead;
   };
 
   list.contains = function(target) {
+    var result = false;
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        result = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return result;
   };
-
   return list;
 };
 
