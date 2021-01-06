df616100d32915cdab19db78256fb33ca1785451
Jordan <jordanace83@gmail.com>
2021-01-06 17:56:49 -0600

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = []; // fix me
 
-  // your code here
-  newTree.children = null;  // fix me
+  // newTree = {value: 5, children: [{value: 6, children: [{value: 8, children: []}]}, {value: 7, children: []}], {addChild: function, contains: function}}
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,11 +13,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
+  //console.log(this);
 };
 
 treeMethods.contains = function(target) {
-};
+  var result = false;
 
+  var innerFunction = function (node) {
+    for (var i = 0; i < node.children.length; i++) {
+      var child = node.children[i];
+      if (child.value === target) {
+        result = true;
+      }
+      if (child.children.length > 0) {
+        innerFunction(child);
+      }
+    }
+  };
+
+  innerFunction(this);
+  return result;
+};
 
 
 /*
