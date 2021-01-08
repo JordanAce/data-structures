17bb574731775498a9aa77145842a147faa0e1ab
Jordan <jordanace83@gmail.com>
2021-01-07 18:47:19 -0600

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+  if (this._storage.includes(item)) {
+    result = true;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+
+  this._storage.splice(index, 1);
+
+
 };
 
 /*
